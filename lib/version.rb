9e831249d8bb3f6442bd51638aa7e94f1aa42bc3
Tom Lea <commit@tomlea.co.uk>
2009-02-20 18:05:03 +0000

    Version bump, adds features: * Support for -p/--update-crontab to allow us to keep the existing crontab as is.
    Generated crontab will look as follows:
    
      0 0 7,21,25,29 * * /a/system/command
    
      ### BEGIN whenever generated crontab ###
      0 0 19 * * /some/whenever/generated/command
      ### END   whenever generated crontab ###
    
      0 0 1,5,9,13,17,21,25,29 * * /sbin/shutdown -r now
    
    Anything outside the ### tags are safe to play with.

--- lib/version.rb
+++ lib/version.rb
@@ -3,7 +3,8 @@ module Whenever
     MAJOR = 0
     MINOR = 1
     TINY  = 5
+    FORK  = 1
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING = [MAJOR, MINOR, TINY, FORK].join('.')
   end
 end unless defined?(Whenever::VERSION)
